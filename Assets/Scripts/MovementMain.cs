1608cb4b1eda588a05e35eb84805ed18b4b00393
davidkapcsandi <98022110+davidkapcsandi@users.noreply.github.com>
2025-02-17 22:36:41 +0000

    Updated movement

--- Assets/Scripts/MovementMain.cs
+++ Assets/Scripts/MovementMain.cs
@@ -1,7 +1,44 @@
 using UnityEngine;
 
-public class MovementMain : MonoBehaviour
+public class CharacterMovement : MonoBehaviour
 {
- 
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    private Rigidbody2D rb;
+    private bool isGrounded;
 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Move();
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    void Move()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(moveX * moveSpeed, moveY * moveSpeed);
+    }
+
+    void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        isGrounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
 }
